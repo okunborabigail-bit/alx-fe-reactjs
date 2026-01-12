fb023fc217dc276f809ff009adba7322bda54b1a
chioma <okunborabigail@gmail.com>
2026-01-12 21:23:59 +0000

    Add WelcomeMessage component and update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,12 @@
-import Welcome from './components/Welcome';
+// src/App.jsx
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage'; // import the component
 function App() {
-  return (
-    <Welcome />
-  );
+    return (
+        <div className="App">
+            <WelcomeMessage /> {/* include the component here */}
+        </div>
+    );
 }
 export default App;
 
