2d8d37062332697dd016baa32df62225daa098b5
chioma <okunborabigail@gmail.com>
2026-01-29 22:14:52 +0000

    Import and render RecipeList in App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,15 @@
-import AddRecipeForm from './components/AddRecipeForm';
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 function App() {
   return (
     <div>
-      <h1 style={{ padding: '20px' }}>Recipe Sharing App</h1>
+      <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
+      <RecipeList />
     </div>
   );
 }
 export default App;
+
+
 
