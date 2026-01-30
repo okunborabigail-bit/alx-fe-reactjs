d80b49620ffc4089781e8ad701903bf4ba594b5b
chioma <okunborabigail@gmail.com>
2026-01-30 23:37:12 +0000

    Initial project setup with .gitignore

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+import Search from "./components/Search";
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+}
+export default App;
+
---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,9 @@
+function Search() {
+  return (
+    <div>
+      <input type="text" placeholder="Search GitHub user..." />
+      <button>Search</button>
+    </div>
+  );
+}
+export default Search;
